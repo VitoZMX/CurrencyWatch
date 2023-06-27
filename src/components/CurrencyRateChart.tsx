c49ee6c91fed6c4680d4474ceccf0ebfed9e2323
VitoZMX <Vi-69@yandex.ru>
2023-06-28 00:34:04 +0300

    feat: add mini Preloader

--- src/components/CurrencyRateChart.tsx
+++ src/components/CurrencyRateChart.tsx
@@ -3,6 +3,7 @@ import {CartesianGrid, Legend, Line, LineChart, Tooltip, XAxis, YAxis} from 'rec
 import {currenciesAPI} from '../api/currenciesAPI'
 import {CurrencyRateChartType} from '../types/types'
 import Typography from '@mui/material/Typography'
+import {Preloader} from './common/Preloader'
 
 type CurrencyRateChartPropsType = {
     id: number
@@ -14,20 +15,6 @@ export function CurrencyRateChart({id, startDate, endDate}: CurrencyRateChartPro
     const [currencyRateChart, setCurrencyRateChart] = React.useState<CurrencyRateChartType[] | null>(null)
     const [loading, setLoading] = useState(true)
 
-    const fetchData = async (id: number, from: string, to: string) => {
-        const data = await currenciesAPI.getRateCurrencyDynamics(id, from, to)
-
-        setCurrencyRateChart(data)
-        console.log(data)
-    }
-
-    /*useEffect(() => {
-        let x = new Date()
-        fetchData(id, new Date('1990-03-11T02:30:00.000').toISOString().split('T')[0], x.toISOString().split('T')[0])
-        //fetchData(id, new Date('1990-03-11T02:30:00.000').toISOString().split('T')[0], x.toISOString().split('T')[0])
-        // fetchData(id, new Date('2000-03-11T02:30:00.000').toISOString().split('T')[0], new Date('2010-03-11T02:30:00.000').toISOString().split('T')[0])
-    }, [id])*/
-
     useEffect(() => {
         currenciesAPI.getRateCurrencyDynamics(id, new Date(startDate).toISOString(), new Date(endDate).toISOString())
             .then(res =>
@@ -36,14 +23,14 @@ export function CurrencyRateChart({id, startDate, endDate}: CurrencyRateChartPro
     }, [id])
 
     if (loading) {
-        return <div>Загрузка...</div>
+        return <Preloader mini={true}/>
     }
 
     return (
         <div>
             {currencyRateChart && currencyRateChart.length > 0 ? (
                 <LineChart
-                    width={570}
+                    width={550}
                     height={300}
                     data={currencyRateChart}
                     margin={{
@@ -65,7 +52,7 @@ export function CurrencyRateChart({id, startDate, endDate}: CurrencyRateChartPro
                         activeDot={{r: 8}}
                     />
                 </LineChart>
-            ) : (<Typography variant="body1" style={{color: '#ee8c0c'}}>Данных по курсу на этот промежуток времени не
+            ) : (<Typography variant="body1" style={{color: '#8884d8'}}>Данных по курсу на этот промежуток времени не
                 найдено!</Typography>)}
         </div>
     )
